cf33bf91db694ee024104b64ba7f67a06470c032
Faaruque <faaruque99@com>
2022-10-10 00:26:42 +0600

    express-app-create

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
+router.get("/", (_req, res) => {
+  res.status(200).json({ message: "Hello World !" });
+});
+
 router.get("/health", (_req, res) => {
   res.status(200).json({ message: "Success" });
 });
